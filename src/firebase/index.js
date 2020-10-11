c4392a3984f94400554dc83948f4a7e345028842
Marco Andrade <alonso_ac93@hotmail.com>
2020-10-10 23:08:11 -0500

    select user account

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -14,6 +14,9 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
 
 export { auth, provider };
 
